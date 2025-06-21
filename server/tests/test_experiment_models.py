19b8f5e203f6aaabe78149994986b5486a9135a5
Ingmar Splitt <orgua@users.noreply.github.com>
2025-06-21 12:19:35 +0200

    fix test that misses a user

--- server/tests/test_experiment_models.py
+++ server/tests/test_experiment_models.py
@@ -2,6 +2,7 @@ import datetime
 
 from shepherd_core.data_models.experiment import Experiment
 from shepherd_server.api_experiment.models import WebExperiment
+from shepherd_server.api_user.models import User
 
 
 async def test_get_next_scheduling(
@@ -12,30 +13,37 @@ async def test_get_next_scheduling(
     assert database_for_tests
     await WebExperiment.delete_all()
     assert await WebExperiment.get_next_scheduling() is None
-
-    one = WebExperiment(experiment=sample_experiment)
-    await one.save_changes()
+    user = await User.by_email("user@test.com")
+    one = WebExperiment(experiment=sample_experiment, owner=user)
+    await one.save()
 
     assert await WebExperiment.get_next_scheduling() is None
 
     one.requested_execution_at = datetime.date(2000, 1, 1)
     await one.save_changes()
 
-    assert await WebExperiment.get_next_scheduling() == one
+    _next = await WebExperiment.get_next_scheduling()
+    assert _next.id == one.id
 
-    two = WebExperiment(experiment=sample_experiment)
-    await two.save_changes()
+    two = WebExperiment(experiment=sample_experiment, owner=user)
+    await two.save()
 
-    assert await WebExperiment.get_next_scheduling() == one
+    _next = await WebExperiment.get_next_scheduling()
+    assert _next.id == one.id
     two.requested_execution_at = datetime.date(2001, 1, 1)
     await two.save_changes()
 
-    assert await WebExperiment.get_next_scheduling() == one
+    _next = await WebExperiment.get_next_scheduling()
+    assert _next.id == one.id
 
     two.requested_execution_at = datetime.date(1999, 1, 1)
     await two.save_changes()
 
+    _next = await WebExperiment.get_next_scheduling()
+    assert _next.id == two.id
+
     two.started_at = datetime.date(1999, 1, 1)
     await two.save_changes()
 
-    assert await WebExperiment.get_next_scheduling() == one
+    _next = await WebExperiment.get_next_scheduling()
+    assert _next.id == one.id

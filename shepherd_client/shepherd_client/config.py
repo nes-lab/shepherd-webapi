68e48ae18fd92cf5821fac9c56280af0193e6b66
Ingmar Splitt <orgua@users.noreply.github.com>
2026-04-20 17:05:09 +0200

    client - use ryaml instead of PyYAML

--- shepherd_client/shepherd_client/config.py
+++ shepherd_client/shepherd_client/config.py
@@ -1,10 +1,9 @@
 import os
 from pathlib import Path
 from typing import Annotated
-from typing import Any
-from typing import Self
+from typing_extensions import Self
 
-import yaml
+import ryaml
 from pydantic import BaseModel
 from pydantic import EmailStr
 from pydantic import Field
@@ -13,8 +12,6 @@ from pydantic import StringConstraints
 from shepherd_core.data_models.base.timezone import local_now
 from shepherd_core.data_models.base.wrapper import Wrapper
 from shepherd_core.logger import log
-from yaml import Node
-from yaml import SafeDumper
 
 PasswordStr = Annotated[str, StringConstraints(min_length=10, max_length=64, pattern=r"^[ -~]+$")]
 # ⤷ Regex = All Printable ASCII-Characters with Space
@@ -24,14 +21,6 @@ def generate_password() -> PasswordStr:
     import exrex
 
     return exrex.getone("[ -~]{64}")
-
-
-def generic2str(dumper: SafeDumper, data: Any) -> Node:
-    """Add a yaml-representation for a specific datatype."""
-    return dumper.represent_scalar("tag:yaml.org,2002:str", str(data))
-
-
-yaml.add_representer(HttpUrl, generic2str, SafeDumper)
 
 
 def get_xdg_config() -> Path:
@@ -50,32 +39,28 @@ class ClientConfig(BaseModel):
     server: HttpUrl = "https://shepherd.cfaed.tu-dresden.de:8000/"
     user_email: EmailStr | None = None
     password: PasswordStr | None = Field(default_factory=generate_password)
+    timeout: int = 3
 
     def to_file(self) -> None:
-        """Store data to yaml in a wrapper."""
+        """Store data to YAML in a wrapper."""
         model_wrap = Wrapper(
             datatype=type(self).__name__,
             created=local_now(),
             parameters=self.model_dump(exclude_unset=True),
-        )
-        model_yaml = yaml.safe_dump(
-            model_wrap.model_dump(exclude_unset=True, exclude_defaults=True),
-            default_flow_style=False,
-            sort_keys=False,
-        )
+        ).model_dump(exclude_unset=True, exclude_defaults=True)
         if not path_shp_config.parent.exists():
             path_shp_config.parent.mkdir(parents=True)
-        with path_shp_config.open("w") as f:
-            f.write(model_yaml)
+        with path_shp_config.open("w", encoding="utf-8") as cfg_file:
+            ryaml.dump(cfg_file, model_wrap)
 
     @classmethod
     def from_file(cls) -> Self:
-        """Load from yaml."""
+        """Load from YAML."""
         if not path_shp_config.exists():
             log.debug("No config found, will use default")
             return cls()
-        with path_shp_config.open() as cfg_file:
-            cfg_dict = yaml.safe_load(cfg_file)
+        with path_shp_config.open(encoding="utf-8") as cfg_file:
+            cfg_dict = ryaml.load(cfg_file)
         cfg_wrap = Wrapper(**cfg_dict)
         if cfg_wrap.datatype != cls.__name__:
             raise ValueError("Data in file does not match the requirement")

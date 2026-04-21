1ac05dc08e399cefd1433e4edca988d0e0d320bd
Ingmar Splitt <orgua@users.noreply.github.com>
2026-04-21 14:36:31 +0200

    add components to content-router

--- shepherd_server/shepherd_server/api_content/router.py
+++ shepherd_server/shepherd_server/api_content/router.py
@@ -6,9 +6,16 @@ from shepherd_core.data_models.content import Firmware
 from shepherd_core.data_models.content import VirtualHarvesterConfig
 from shepherd_core.data_models.content import VirtualSourceConfig
 from shepherd_core.data_models.content import VirtualStorageConfig
+from shepherd_core.data_models.testbed import GPIO
+from shepherd_core.data_models.testbed import MCU
+from shepherd_core.data_models.testbed import Cape
+from shepherd_core.data_models.testbed import Observer
+from shepherd_core.data_models.testbed import Target
+from shepherd_core.data_models.testbed import Testbed
 from shepherd_core.testbed_client import tb_client
 
 router = APIRouter(prefix="/content", tags=["Content"])
+# TODO: rename to resources
 
 content_types = [
     EnergyEnvironment,
@@ -16,27 +23,39 @@ content_types = [
     VirtualSourceConfig,
     VirtualStorageConfig,
     Firmware,
+    # TODO: added tb-components - temp fix to replace fixture client
+    Cape,
+    GPIO,
+    MCU,
+    Observer,
+    Target,
+    Testbed,
 ]
-content_names = [content.__name__ for content in content_types]
+content_names = [content.__name__.lower() for content in content_types]
 
 
 @router.get("")
 async def list_content_types() -> list[str]:
-    return sorted(content_names)
+    return sorted(content.__name__ for content in content_types)
 
 
 @router.get("/{content}")
-async def list_content_by_type(content: str) -> list[str]:
+async def list_content_by_type(content: str) -> dict[int, str]:
+    content = content.lower()
     if content not in content_names:
         raise HTTPException(404, "Not Found")
     data = tb_client.list_content_ids(content)
     models = [tb_client.get_content_item(content, uid=uid) for uid in data]
     models = [model for model in models if model.get("deprecated") is None]
-    models = [model for model in models if model.get("visible2all")]  # TODO: or user identical
-    return sorted([model.get("name") for model in models])
-    # -> moved from sorted dict[ID,name] (that was resorted by fastapi) to just list of names
+    models = [
+        model for model in models if model.get("visible2all", True)
+    ]  # TODO: or user identical
+    return {int(model.get("id", 0)): str(model.get("name")) for model in models}
+    # -> moved from sorted dict[ID,name] (that was resorted by fastapi) to
+    #      - just list of names (was hardly usable by TestbedClient)
+    #      - unsorted dict[ID, name]
     # TODO: replace fixture-endpoints by database-endpoints
-    # TODO: include user/group-data
+    # TODO: include data of user/group
 
     # TODO: add setter-endpoint
     # TODO: add modifiers-endpoint
@@ -44,6 +63,7 @@ async def list_content_by_type(content: str) -> list[str]:
 
 @router.get("/{content}/{name}")
 async def get_content_by_type_and_name(content: str, name: str) -> ContentModel:
+    content = content.lower()
     if content not in content_names:
         raise HTTPException(404, "Not Found")
     try:
